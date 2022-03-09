bd8658fd7e9b3273b48e25238c6c7bb10f7c6328
Emmanuel Murairi <emurairi18@alastudents.org>
2022-03-09 20:22:39 +0400

    get data from the formA

---
+++ form.js
@@ -0,0 +1,14 @@
+
+function signup(){
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var paypal = document.getElementById("paypal").value;
+    var visa = document.getElementById("visa").value;
+
+    console.log(username);
+    console.log(password);
+    console.log(paypal);
+    console.log(visa);
+
+}
